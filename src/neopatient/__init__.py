acd84e70f7f315a05bbb56ae5a9255271e35f0a7
Shiva Kaul <shiva.kaul@gmail.com>
2025-11-25 07:55:00 -0500

    [popmedqa] write generated cohorts properly

--- src/neopatient/__init__.py
+++ src/neopatient/__init__.py
@@ -1,11 +1,9 @@
 __version__ = "0.1.0"
-from .matcher import find_best_matching_code
-from .database import setup_databases, load_chroma_client
 from .generator import (
     synthesize_patient,
     synthesize_cohort,
     synthesize_cohort_with_state_file,
 )
-from .models import CohortSpec, RecordType
+from .models import Cohort, CohortSpec, RecordType
 
-__all__ = ["find_best_matching_code", "setup_databases", "load_chroma_client", "synthesize_patient", "synthesize_cohort", "synthesize_cohort_with_state_file", "CohortSpec", "RecordType"]
+__all__ = ["synthesize_patient", "synthesize_cohort", "synthesize_cohort_with_state_file", "Cohort", "CohortSpec", "RecordType"]
