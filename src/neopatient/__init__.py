0fe726c3246a77a40a1f3f00adb7b45727b4839f
Shiva Kaul <shiva.kaul@gmail.com>
2025-11-24 22:01:45 -0500

    [neopatient] export types

--- src/neopatient/__init__.py
+++ src/neopatient/__init__.py
@@ -6,5 +6,6 @@ from .generator import (
     synthesize_cohort,
     synthesize_cohort_with_state_file,
 )
+from .models import CohortSpec, RecordType
 
-__all__ = ["find_best_matching_code", "setup_databases", "load_chroma_client", "synthesize_patient", "synthesize_cohort", "synthesize_cohort_with_state_file"]
+__all__ = ["find_best_matching_code", "setup_databases", "load_chroma_client", "synthesize_patient", "synthesize_cohort", "synthesize_cohort_with_state_file", "CohortSpec", "RecordType"]
